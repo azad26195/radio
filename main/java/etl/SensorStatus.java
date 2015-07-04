4b9418db93561cac245a7828f8b9dddfe63ea1ea
azad <azad26195@gmail.com>
2015-07-04 12:50:26 +0530

    Fix-Sensor Status

--- main/java/etl/SensorStatus.java
+++ main/java/etl/SensorStatus.java
@@ -4,11 +4,13 @@ public class SensorStatus{
 	public String from = "";
 	public String to = "";
 	public String status = "";
+	public Long diff;
 
-	public  SensorStatus(String from,String to,String status){
+	public  SensorStatus(String from,String to, Long diff, String status){
 	 	this.from = from;
 		this.to = to;
 		this.status = status;
+		this.diff = diff;
 	}
 
 }
--- main/java/etl/ServerBuffer.java
+++ main/java/etl/ServerBuffer.java
@@ -33,6 +33,7 @@ import java.util.TimeZone;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
 
 import org.joda.time.DateTimeComparator;
@@ -51,20 +52,28 @@ public class ServerBuffer extends BaseOperation implements Buffer
     super( 1, fieldDeclaration );
   }
 
-  public Date toDateString(String timestampString){
+  public Date toDateString(String timestampString) {
+    return new Date(new Long(timestampString.trim() + "000"));
+  }
 
-      Long timestamp = new Long(timestampString.trim());
-      Date date = new Date(timestamp*1000L); // *1000 is to convert seconds to milliseconds
+  public DateTime toDateTime(String timestampString){
+
+      Long timestamp = new Long(timestampString.trim() + "000");
+      return new DateTime(timestamp,DateTimeZone.UTC); // *1000 is to convert seconds to milliseconds
       // SimpleDateFormat sdf = new SimpleDateFormat("HH:mm"); // the format of your date
       // sdf.setTimeZone(DateTimeZone.UTC); // give a timezone reference for formating (see comment at the bottom
       // Date formattedDate = sdf.format(date);
 
-      return date;
+      // return date;
 
     }
 
-    public String HHMM(String timestampString){
-      
+    public String toHHMM(String timestampString){
+      SimpleDateFormat sdf = new  SimpleDateFormat("HH:MM:ss a");
+      sdf.setTimeZone(TimeZone.getTimeZone("GMT+5.30"));
+      DateTime date = toDateTime(timestampString.trim());
+
+      return date.getHourOfDay() + ":" + date.getMinuteOfDay();    // return sdf.format(toDateString(timestampString.trim()));
     }
 
 
@@ -76,13 +85,10 @@ public class ServerBuffer extends BaseOperation implements Buffer
 
     // get all the current argument values for this grouping
 
-    String startTimeString = "";
-    String currentTimeString = "";
-    String endTimeString = "";
-    String currentStatus = "ON";
-    boolean isStatusChange = false;
-    boolean isFirstTime = true;
-
+    String i_startTimeString = "";
+    String p_currentTimeString = "";
+    String c_endTimeString = "";
+    Long diff = 0L;
     Date endTime = new Date();
     Date currentTime = new  Date();
 
@@ -94,67 +100,92 @@ public class ServerBuffer extends BaseOperation implements Buffer
     ArrayList<SensorStatus> sensorList = new ArrayList();
 
 
-    if(arguments.hasNext() & isFirstTime){
-      currentSensor = arguments.next();
-      startTimeString =  currentSensor.getString(2);
-      currentSensor = arguments.next();
-      endTimeString = currentSensor.getString(2);
-      currentTimeString = startTimeString;
-    }
+    // if(arguments.hasNext() & isFirstTime){
+    //   currentSensor = arguments.next();
+    //   i_startTimeString =  currentSensor.getString(2);
+    //   currentSensor = arguments.next();
+    //   c_endTimeString = currentSensor.getString(2);
+    //   p_currentTimeString = i_startTimeString;
+    // }
 
+      currentSensor = arguments.next();
+      i_startTimeString = currentSensor.getString(2);
+      p_currentTimeString = i_startTimeString;
     while( arguments.hasNext())
     {
-      isFirstTime = false;
 
+      currentSensor = arguments.next();
+      c_endTimeString = currentSensor.getString(2);
+      currentTime =toDateString(p_currentTimeString);
+      endTime = toDateString(c_endTimeString);
+        // System.out.println(endTime.getTime() - currentTime.getTime())
+      diff = endTime.getTime() - currentTime.getTime();
+      if(diff > 2000 * 60){
 
-
-      currentTime =toDateString(currentTimeString);
-      endTime = toDateString(endTimeString);
-      
-
-      if((endTime.getTime() - currentTime.getTime() ) < 2000){
-        currentSensor = arguments.next();
-        currentTimeString = endTimeString;
-        endTimeString  = currentSensor.getString(2);
-
-        isStatusChange = false;
+        sensorList.add(new SensorStatus(toHHMM(i_startTimeString),toHHMM(p_currentTimeString), diff,"ON"));
+        sensorList.add(new SensorStatus(toHHMM(p_currentTimeString),toHHMM(c_endTimeString), diff,"OFF"));
+        i_startTimeString = c_endTimeString;
+        p_currentTimeString=i_startTimeString;
 
       }
       else{
 
-        sensorList.add(new SensorStatus(startTimeString ,endTimeString,currentStatus));
-
-
-        isStatusChange = true;
+        p_currentTimeString = c_endTimeString;
       }
 
-      if(isStatusChange){
-        currentSensor = arguments.next();
-        endTimeString = currentSensor.getString(2);
-       startTimeString = currentTimeString;
 
-       isStatusChange = false;
-       currentStatus = (currentStatus == "OFF")?"ON":"OFF";
+     // f isFirstTime = false;
 
-     }
+
+
+      
+
+     //  if((endTime.getTime() - currentTime.getTime() ) < 2000){
+     //    currentSensor = arguments.next();
+     //    p_currentTimeString = c_endTimeString;
+     //    c_endTimeString  = currentSensor.getString(2);
+
+     //    isStatusChange = false;
+
+     //  }
+     //  else{
+
+     //    sensorList.add(new SensorStatus(i_startTimeString ,c_endTimeString,currentStatus));
+
+
+     //    isStatusChange = true;
+     //  }
+
+     //  if(isStatusChange){
+     //    currentSensor = arguments.next();
+     //    c_endTimeString = currentSensor.getString(2);
+     //   i_startTimeString = p_currentTimeString;
+
+     //   isStatusChange = false;
+     //   currentStatus = (currentStatus == "OFF")?"ON":"OFF";
+
+     // }
 
 
    }
 
-   isFirstTime = false;
+   sensorList.add(new SensorStatus(toHHMM(i_startTimeString),toHHMM(c_endTimeString), diff,"ON"));
+
+
+   // isFirstTime = false;
 
    Iterator<SensorStatus> resultList = sensorList.iterator();
 
     while(resultList.hasNext()){
       SensorStatus results = resultList.next();
       System.out.println(results.from);
-      Tuple result = new Tuple(currentSensor.getString(1), "", results.status) ;
+      Tuple result = new Tuple(currentSensor.getString(1),results.from + "-" + results.to + " \tDiff " + results.diff, results.status) ;
       bufferCall.getOutputCollector().add(result);
 
     }
     // // create a Tuple to hold our result values
 
-    // return the result Tuple
+  // return the result Tuple
  }
 
 
